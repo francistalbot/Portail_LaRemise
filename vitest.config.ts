d7d7fd270e9f2049ab125972ed4d5c3217c84727
francistalbot <francistalbot@hotmail.com>
2025-10-29 21:37:15 -0400

    add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,38 @@
+// vitest.config.ts
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+    plugins: [
+        react({
+            include: [/\.js$/, /\.jsx$/, /\.ts$/, /\.tsx$/],
+        }),
+    ],
+    test: {
+        globals: true,
+        css: true,
+        environment: "jsdom",
+        setupFiles: "./vitest.setup.ts",
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "lcov", "html"],
+            reportsDirectory: "./coverage",
+            exclude: [
+                "node_modules/",
+                "src/test/",
+                "**/*.d.ts",
+                "**/*.config.*",
+                "**/coverage/**",
+                "resources/js/bootstrap.ts",
+                "resources/js/app.tsx",
+            ],
+            include: ["resources/js/**/*.{ts,tsx}"],
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./resources/js"),
+        },
+    },
+});
