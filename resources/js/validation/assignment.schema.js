8342d7ec015b90b8d0b10970f716a07aaaf66056
francistalbot <francistalbot@hotmail.com>
2025-11-18 15:02:24 -0500

    zod assignment transform string iso en date

--- resources/js/validation/assignment.schema.js
+++ resources/js/validation/assignment.schema.js
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
- export const AssignmentSchema = z.object({
-    Id: z.number().int().positive(), // un nombre entier > 0
-    Date: z.date(),
-    PosteID: z.number().int().positive(),
-    BenevoleID: z.number().int().positive(),
-    EventID: z.number().int().positive()
- }).strict(); // Aucune clé surprise n’est acceptée
+export const AssignmentSchema = z
+    .object({
+        Id: z.number().int().positive(), // un nombre entier > 0
+        Date: z.coerce.date(), // Transforme string ISO en Date
+        PosteID: z.number().int().positive(),
+        BenevoleID: z.number().int().positive(),
+        EventID: z.number().int().positive(),
+    })
+    .strict(); // Aucune clé surprise n'est acceptée
