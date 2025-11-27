8947e378fba418035a7fa2f8eeadce8eb3a2ce3f
francistalbot <francistalbot@hotmail.com>
2025-11-27 17:56:06 -0500

    section

---
+++ resources/js/Components/HeroSection.tsx
@@ -0,0 +1,13 @@
+export const HeroSection = () => {
+    return (
+        <div className="relative bg-white overflow-hidden">
+            <div className="max-w-7xl mx-auto">
+                <img
+                    className="w-full h-auto"
+                    src="/images/hero.png"
+                    alt="Hero Image"
+                />
+            </div>
+        </div>
+    );
+};
